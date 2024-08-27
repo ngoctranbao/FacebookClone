34c936d25ca21b4827734afa3e55219e11adf6f3
ngoctranbao <ngoc110904@gmail.com>
2024-08-27 20:19:15 +0700

    dash bar ui

--- fe/src/components/DashBar.js
+++ fe/src/components/DashBar.js
@@ -1,31 +1,39 @@
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faBell, faDesktop, faHouse, faMagnifyingGlass, faUserGroup,faStore } from '@fortawesome/free-solid-svg-icons';
+import { faBell, faDesktop, faHouse, faMagnifyingGlass, faUserGroup,faStore, faUsersLine, faGripVertical } from '@fortawesome/free-solid-svg-icons';
 import { faFacebook, faFacebookMessenger } from'@fortawesome/free-brands-svg-icons';
 import './DashBar.css'
-import { Row } from 'antd';
+import { Row, Col } from 'antd';
 
 const Dashboard = () => {
     return (
-      <Row style={{width: "100%", position: "absolute", backgroundColor: "yellow", display: "flex", alignItems: "center", justifyContent: "center", position: "absolute"}}>
-          <FontAwesomeIcon icon={faFacebook}/>
-          <FontAwesomeIcon icon={faMagnifyingGlass} />
+      <Row className='dash_bar'>
+          <Col>
+            <FontAwesomeIcon icon={faFacebook} className='facebook-item'/>
+            <FontAwesomeIcon icon={faMagnifyingGlass}  className='dashbar-item'/>
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faHouse}  className='navigate-item'/>
+            <FontAwesomeIcon icon={faUserGroup}  className='navigate-item'/>
+            <FontAwesomeIcon icon={faDesktop}  className='navigate-item'/>
+            <FontAwesomeIcon icon={faStore}  className='navigate-item'/>
+            <FontAwesomeIcon icon={faUsersLine}  className='navigate-item'/>
+          </Col>
+          <Col>
+            <FontAwesomeIcon icon={faGripVertical}  className='dashbar-item'/>
+            <FontAwesomeIcon icon={faFacebookMessenger}  className='dashbar-item'/>
+            <FontAwesomeIcon icon={faBell}  className='dashbar-item'/>
+          </Col>
           {/* <Link to='/'>
-            <FontAwesomeIcon icon={faHouse} />
           </Link>
           <Link to='/friends'>
-            <FontAwesomeIcon icon={faUserGroup} />
           </Link>
           <Link to='/watch'>
-            <FontAwesomeIcon icon={faDesktop} />
           </Link>
           <Link to='/*'>
-            <FontAwesomeIcon icon={faStore} />
           </Link>
           <Link to='/messages'>
-              <FontAwesomeIcon icon={faFacebookMessenger} />
           </Link> */}
-          <FontAwesomeIcon icon={faBell} />
       </Row>
     )
 }
